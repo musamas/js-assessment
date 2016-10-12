9dea961d25922e23f3a3a0030868dfdecc4cd41f
musamas <musamas@gmail.com>
2016-10-11 23:57:35 -0400

    Update async.js
    Implemented async with promises.

--- app/async.js
+++ app/async.js
@@ -3,11 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+      var def = $.Deferred();
+      def.resolve(value);
+      return def.promise();
     },
 
     manipulateRemoteData : function(url) {
-
+      var def = $.Deferred();
+      var names = [];
+      $.ajax(url).then(function(data) {
+        $.map(data.people, function(person) {
+          names.push(person.name);
+        });
+        
+        def.resolve(names.sort());
+      });
+      
+      return def.promise();
     }
   };
 });
